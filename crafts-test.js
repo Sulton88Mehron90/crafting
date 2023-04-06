d5a513a2c8d0f070569fdbf7ee5ecf2633afc623
Kayla Wood <kaylaewood@gmail.com>
2023-04-06 10:53:00 -0700

    Initial commit

---
+++ crafts-test.js
@@ -0,0 +1,8 @@
+var assert = require('chai').assert;
+var {  } = require('./crafts');
+
+describe('Elevator', function() {
+  it.skip('should create a new craft project', function() {
+    assert.equal(true, true);
+  });
+});
